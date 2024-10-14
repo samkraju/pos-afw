e1060835b7cdbdfb2abdf477420f9ab8b7c7671c
loveena <loveena@localhost>
2024-10-14 23:50:43 +0400

    Renamed the package script to scriptForTestDummy

--- src/test/java/scriptForTestDummy/Demo1.java
+++ src/test/java/scriptForTestDummy/Demo1.java
@@ -1,4 +1,4 @@
-package script;
+package scriptForTestDummy;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
--- src/test/java/scriptForTestDummy/TestDemo.java
+++ src/test/java/scriptForTestDummy/TestDemo.java
@@ -1,15 +1,19 @@
-package script;
+package scriptForTestDummy;
 //SInce we have given @Optional values and this class extends base test we can run them individually without
 //having to run testNG.xml file
 
 import org.testng.annotations.Test;
+import org.testng.Assert;
 import org.testng.Reporter;
 import generic.BaseTest;
+import generic.Utility;
 
-public class TestDemo2 extends BaseTest {
+public class TestDemo extends BaseTest {
 	@Test
 	public void test1() {
-		Reporter.log("Loveena running test1....", true);
+		String v = Utility.readFromExcel(xlpath, "sheet1", 0, 0);
+		Reporter.log("test1...."+v, true);
+	//	Assert.fail();
 	}
 
 }
