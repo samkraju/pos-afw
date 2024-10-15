63c747a8f4f9d8776c7c8d4bccc9717bdd50f577
loveena <loveena@localhost>
2024-10-15 18:26:37 +0400

    in testNG.xml grid is set to NO

--- src/test/java/scriptForTestDummy/TestDemo.java
+++ src/test/java/scriptForTestDummy/TestDemo.java
@@ -1,19 +1,19 @@
-package scriptForTestDummy;
-//SInce we have given @Optional values and this class extends base test we can run them individually without
-//having to run testNG.xml file
-
-import org.testng.annotations.Test;
-import org.testng.Assert;
-import org.testng.Reporter;
-import generic.BaseTest;
-import generic.Utility;
-
-public class TestDemo extends BaseTest {
-	@Test
-	public void test1() {
-		String v = Utility.readFromExcel(xlpath, "sheet1", 0, 0);
-		Reporter.log("test1...."+v, true);
-	//	Assert.fail();
-	}
-
-}
+//package scriptForTestDummy;
+////SInce we have given @Optional values and this class extends base test we can run them individually without
+////having to run testNG.xml file
+//
+//import org.testng.annotations.Test;
+//import org.testng.Assert;
+//import org.testng.Reporter;
+//import generic.BaseTest;
+//import generic.Utility;
+//
+//public class TestDemo extends BaseTest {
+//	@Test
+//	public void test1() {
+//		String v = Utility.readFromExcel(xlpath, "sheet1", 0, 0);
+//		Reporter.log("test1...."+v, true);
+//	//	Assert.fail();
+//	}
+//
+//}
--- src/test/java/scriptForTestDummy/TestDemo2.java
+++ src/test/java/scriptForTestDummy/TestDemo2.java
@@ -1,15 +1,15 @@
-package scriptForTestDummy;
-//SInce we have given @Optional values and this class extends base test we can run them individually without
-//having to run testNG.xml file
-
-import org.testng.annotations.Test;
-import org.testng.Reporter;
-import generic.BaseTest;
-
-public class TestDemo2 extends BaseTest {
-	@Test
-	public void test1() {
-		Reporter.log("Loveena running test1....", true);
-	}
-
-}
+//package scriptForTestDummy;
+////SInce we have given @Optional values and this class extends base test we can run them individually without
+////having to run testNG.xml file
+//
+//import org.testng.annotations.Test;
+//import org.testng.Reporter;
+//import generic.BaseTest;
+//
+//public class TestDemo2 extends BaseTest {
+//	@Test
+//	public void test1() {
+//		Reporter.log("Loveena running test1....", true);
+//	}
+//
+//}
